075eba90d9a4fb9cd92230aad5e6a03af21eb1ef
XxXEnderDragonSlayer07XxX/draken <tor.wallberg@elev.ga.ntig.se>
2025-08-27 11:18:32 +0200

    moved html to php

--- index.php
+++ index.php
@@ -9,6 +9,24 @@
     <nav>
         <h1>Choose gamemode</h1>
     </nav>
+
+    	
+<h1>sänkig skäpp</h1>	
+
+<p>Klicka på ruta för att skjuta, öändligt med skott!!! om rutan är grå så har du missat, om rutan blir röd så har du träffat en båt.</p>
+<p>Du har vunnit så här många gånger -</p>
+<p>Du har förlorat så här många gånger -</p>
+
+<div id="gameboard">
+</div>
+
+<script type="text/javascript" src="battleship.js"></script>
+
+<form action="welcome.php" method="POST">
+Username:<input type="text" name="name"><br>
+<input type="submit">
+</form>
+
 <?php
 $servername = "localhost";
 $username = "ntigskov_websrv2-ak";
